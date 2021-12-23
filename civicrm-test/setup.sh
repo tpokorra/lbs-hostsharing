92b51e1bd3ddf10306de446c15825512c1b5a150
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-12-23 06:34:47 +0100

    there is no need for ansible password

--- civicrm-test/setup.sh
+++ civicrm-test/setup.sh
@@ -5,13 +5,6 @@ git clone https://github.com/tpokorra/Hostsharing-Ansible-CiviCRM.git || exit -1
 cd Hostsharing-Ansible-CiviCRM
 cp ~/.ssh/civicrm.inventory my.inventory
 
-# see https://stackoverflow.com/a/29213873/1632368
-# ansible-vault create secret
-#   with content: ansible_become_pass: mypassword
-# password stored in vault.txt
-cp ~/.ssh/vault.txt vault.txt
-cp ~/.ssh/hs_secret secret
-
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa_cronjob
 
--- mastodon-test/setup.sh
+++ mastodon-test/setup.sh
@@ -5,16 +5,9 @@ git clone https://github.com/tpokorra/Hostsharing-Ansible-Mastodon.git || exit -
 cd Hostsharing-Ansible-Mastodon
 cp ~/.ssh/mastodon.inventory my.inventory
 
-# see https://stackoverflow.com/a/29213873/1632368
-# ansible-vault create secret
-#   with content: ansible_become_pass: mypassword
-# password stored in vault.txt
-cp ~/.ssh/vault.txt vault.txt
-cp ~/.ssh/hs_secret secret
-
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa_cronjob
 
-ansible-playbook -i my.inventory playbook-uninstall.yml --vault-password-file=vault.txt || exit -1
-ansible-playbook -i my.inventory playbook-mastodon.yml --vault-password-file=vault.txt || exit -1
-ansible-playbook -i my.inventory playbook-uninstall.yml --vault-password-file=vault.txt || exit -1
+ansible-playbook -i my.inventory playbook-uninstall.yml || exit -1
+ansible-playbook -i my.inventory playbook-mastodon.yml || exit -1
+ansible-playbook -i my.inventory playbook-uninstall.yml || exit -1
