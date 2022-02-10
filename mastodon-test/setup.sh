2a09829abdca91959fdec529012de0bc0394c676
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-02-10 07:45:37 +0100

    use yml inventory for mastodon

--- mastodon-test/setup.sh
+++ mastodon-test/setup.sh
@@ -3,11 +3,11 @@
 apt-get -y install ansible git || exit -1
 git clone https://github.com/tpokorra/Hostsharing-Ansible-Mastodon.git || exit -1
 cd Hostsharing-Ansible-Mastodon
-cp ~/.ssh/mastodon.inventory my.inventory
+cp ~/.ssh/mastodon.inventory.yml my.inventory.yml
 
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa_cronjob
 
-ansible-playbook -i my.inventory playbook-uninstall.yml || exit -1
-ansible-playbook -i my.inventory playbook-mastodon.yml || exit -1
-ansible-playbook -i my.inventory playbook-uninstall.yml || exit -1
+ansible-playbook -i my.inventory.yml playbook-uninstall.yml || exit -1
+ansible-playbook -i my.inventory.yml playbook-mastodon.yml || exit -1
+ansible-playbook -i my.inventory.yml playbook-uninstall.yml || exit -1
