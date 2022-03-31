b66525e227c209328bc0e664b9af7e8cc5edb044
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-03-31 09:58:05 +0200

    use ssh multiplexing to avoid triggering the ssh rate limit

--- civicrm-test/setup.sh
+++ civicrm-test/setup.sh
@@ -12,6 +12,14 @@ ssh-add ~/.ssh/id_rsa_cronjob
 export ANSIBLE_SSH_ARGS="-o ServerAliveInterval=50"
 # to avoid error: Failed to connect to the host via ssh: ssh: connect to host example.org port 22: Cannot assign requested address
 export ANSIBLE_SSH_RETRIES=10
+# see https://wiki.hostsharing.net/index.php/SSH_Rate_Limit
+cat >> $HOME/.ssh/config << FINISH
+Host *.hostsharing.net
+ControlPath ~/.ssh/cm-%r@%h:%p
+ControlMaster auto
+ControlPersist 10m
+FINISH
+
 ansible-playbook -i my.inventory.yml playbook-uninstall.yml || exit -1
 ansible-playbook -i my.inventory.yml playbook-install.yml || exit -1
 ansible-playbook -i my.inventory.yml playbook-update.yml || exit -1
