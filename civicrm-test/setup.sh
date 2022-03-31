b117aeaf080eae42963688adebc548727c4f132a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-03-31 06:14:27 +0200

    set ANSIBLE_SSH_RETRIES=10

--- civicrm-test/setup.sh
+++ civicrm-test/setup.sh
@@ -10,6 +10,8 @@ ssh-add ~/.ssh/id_rsa_cronjob
 
 # avoid error: Shared connection to example.org closed. Terminated. MODULE FAILURE See stdout/stderr for the exact error
 export ANSIBLE_SSH_ARGS="-o ServerAliveInterval=50"
+# to avoid error: Failed to connect to the host via ssh: ssh: connect to host example.org port 22: Cannot assign requested address
+export ANSIBLE_SSH_RETRIES=10
 ansible-playbook -i my.inventory.yml playbook-uninstall.yml || exit -1
 ansible-playbook -i my.inventory.yml playbook-install.yml || exit -1
 ansible-playbook -i my.inventory.yml playbook-update.yml || exit -1
