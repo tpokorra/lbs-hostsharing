bed101134d9ecec7b69744e096901d2ede5f0e70
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-08-09 17:02:43 +0200

    run the civicrm test always against the latest release that is not .0

--- civicrm-test/setup.sh
+++ civicrm-test/setup.sh
@@ -5,8 +5,11 @@ git clone https://github.com/tpokorra/Hostsharing-Ansible-CiviCRM.git || exit -1
 cd Hostsharing-Ansible-CiviCRM
 cp ~/.ssh/civicrm.inventory.yml my.inventory.yml
 
-# use the civicrm version from the inventory template
-cat inventory-template.yml | grep "civicrm_version" >> my.inventory.yml
+# read civicrm version from latest github tag that is not a .0 release
+civicrm_version=`curl https://github.com/civicrm/civicrm-core/tags --silent | grep "/civicrm/civicrm-core/releases/tag/" | grep -v '.0">' | head -n 1 | awk -F'"' '{print $2}' | awk -F'/' '{print $NF}'`
+echo "using civicrm_version " $civicrm_version
+echo "civicrm_version: $civicrm_version" >> my.inventory.yml
+# use the composer version from the inventory template
 cat inventory-template.yml | grep "composer_version" >> my.inventory.yml
 
 eval `ssh-agent`
